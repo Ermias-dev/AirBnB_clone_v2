72e54f52272b365c0700be48c6b49c4af0988420
Ermias-dev <edessalegne@gmail.com>
2021-09-09 12:35:02 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
-if __name__ == "__main__":
-    for i in dir(hidden_4):
-    if i[:2] != "__":
-    print(i)
+
+
+def principal():
+        for i in dir(hidden_4):
+                    if not (i[0] == '_' and i[1] == '_'):
+                                    print(i)
+
+                                    if __name__ == "__main__":
+                                            principal()
+                                            
