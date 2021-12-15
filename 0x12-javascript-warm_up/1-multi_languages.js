cd73d35f5b3733641ed205a17030572ad5690e69
Ermias-dev <edessalegne@gmail.com>
2021-12-15 00:53:33 -0800

    main

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-
-/**
- * a script that prints three lines
- */
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+// three languages
+const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(myVar);
